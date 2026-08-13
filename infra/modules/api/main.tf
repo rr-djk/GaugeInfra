4bd3ec2cf3c908af1e8d384aaa46e4f089d89ed7
Roger-Richard_Djuiko'o <djuiko_o_nguimezap.roger-richard@courrier.uqam.ca>
2026-08-13 00:56:48 -0400

    feat: limiter le débit de l'api gateway
    Ajouter un throttling par défaut au stage de l'API Gateway
    (burst 10, rate 5/s) pour protéger la Lambda orchestrateur
    d'une surcharge en aval.

--- infra/modules/api/main.tf
+++ infra/modules/api/main.tf
@@ -8,4 +8,9 @@ resource "aws_apigatewayv2_stage" "this" {
   api_id      = aws_apigatewayv2_api.this.id
   name        = var.stage_name
   auto_deploy = true
+
+  default_route_settings {
+    throttling_burst_limit = 10
+    throttling_rate_limit  = 5
+  }
 }
